0ea10c1f9123dd177a5c6dee065b27dbfbe9fbcd
Scuola <Scuola@Lollo01.homenet.telecomitalia.it>
2017-10-12 16:27:09 +0200

    Aggiunta di un metodo per registrare le operazioni

--- src/model/Conto.java
+++ src/model/Conto.java
@@ -18,5 +18,19 @@ public class Conto {
         cf = new ContoFile();
         dati = cf.leggiDati();
     }
+    
+    public void registraOperazione(Operazione op){
+        //istanza nuovo array +1 rispetto all'esistente
+        Operazione[] dati2 = new Operazione[dati.length + 1];
+        //copio tutti i dati nel nuovo array
+        for (int i = 0; i < dati.length; i++) {
+            dati2[i] = dati[i];
+        }
+        //assegno op all'ultimo posto dell'array nuovo
+        dati2[dati2.length - 1] = op;
+        //assegno il nuovo array a dati
+        dati = dati2;
+        cf.scriviDati(dati);
+    }
 
 }
